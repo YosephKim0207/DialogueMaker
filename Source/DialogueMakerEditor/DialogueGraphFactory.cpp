e2bd58d390cfd3e83d65a9645c31465953d2fb29
YosephKim <kjs940207@gmail.com>
2025-07-12 18:33:37 +0900

    불필요한 코드주석 제거

--- Source/DialogueMakerEditor/DialogueGraphFactory.cpp
+++ Source/DialogueMakerEditor/DialogueGraphFactory.cpp
@@ -2,11 +2,7 @@
 
 
 #include "DialogueGraphFactory.h"
-
-#include "DialogueEdGraphNode.h"
-#include "DialogueEdGraphSchema.h"
 #include "DialogueMaker/DialogueGraph.h"
-#include "Kismet2/BlueprintEditorUtils.h"
 
 UDialogueGraphFactory::UDialogueGraphFactory(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
@@ -20,20 +16,5 @@ UObject* UDialogueGraphFactory::FactoryCreateNew(UClass* InClass, UObject* InPar
 {
 	UDialogueGraph* NewAsset = NewObject<UDialogueGraph>(InParent, InName, Flags);
 
-	// Editor Graph 생성 및 할당
-	// NewAsset->Graph = Cast<UDialogueRuntimeGraph>(FBlueprintEditorUtils::CreateNewGraph(NewAsset, NAME_None, UDialogueEdGraph::StaticClass(), UDialogueEdGraphSchema::StaticClass()));
-	// NewAsset->Graph->bAllowDeletion = false;
-
-	// FEdGraphSchemaAction_NewNode를 사용해 첫 노드를 생성하고 그래프에 추가
-	// UEdGraph* Graph = NewAsset->Graph;
-	// const UEdGraphSchema* Schema = Graph->GetSchema();
-	// FGraphNodeCreator<UDialogueEdGraphNode> NodeCreator(*Graph);
-	// UDialogueEdGraphNode* RootNode = NodeCreator.CreateNode();
-	// RootNode->Dialogue.DialogueText = FText::FromString(TEXT("시작 대화")); // 기본 텍스트 설정
-	// Schema->SetNodePosition(RootNode, FVector2D(0, 0));
-	// NodeCreator.Finalize();
-
-	// Graph->AddNode(RootNode);
-	
 	return NewAsset;
 }
