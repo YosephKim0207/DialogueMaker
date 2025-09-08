a6d8fbadb1eaffb77f0f6f29e0d48a9701ee858f
YosephKim <kjs940207@gmail.com>
2025-09-08 14:45:08 +0900

    DialogueNodeInfo 내 pubilc 변수들 private으로 변경 및 그에 맞게 관련 코드 수정

--- Source/DialogueMaker/DialogueNodeInfo.cpp
+++ Source/DialogueMaker/DialogueNodeInfo.cpp
@@ -1,5 +1,30 @@
 #include "DialogueNodeInfo.h"
 
+const FText& UDialogueNodeInfo::GetTitle() const
+{
+	return Title;
+}
+
+const FText& UDialogueNodeInfo::GetDialogueText() const
+{
+	return DialogueText;
+}
+
+const TArray<FDialogueChoice>& UDialogueNodeInfo::GetDialogueChoices() const
+{
+	return DialogueChoices;
+}
+
+void UDialogueNodeInfo::AddDialogueChoice(const FDialogueChoice& DialogueChoice)
+{
+	DialogueChoices.Add(DialogueChoice);
+}
+
+void UDialogueNodeInfo::RemoveDialogueChoiceAt(int32 Index)
+{
+	DialogueChoices.RemoveAt(Index);
+}
+
 bool UDialogueNodeInfo::IsDialogueAlreadyShown() const
 {
 	return bIsShown;
--- Source/DialogueMaker/DialogueNodeInfo.h
+++ Source/DialogueMaker/DialogueNodeInfo.h
@@ -13,28 +13,43 @@ class DIALOGUEMAKER_API UDialogueNodeInfo : public UDialogueNodeInfoBase
 	GENERATED_BODY()
 
 public:
+	UFUNCTION(BlueprintCallable)
+	const FText& GetTitle() const;
+	
+	UFUNCTION(BlueprintCallable)
+	const FText& GetDialogueText() const;
+
+	UFUNCTION(BlueprintCallable)
+	const TArray<FDialogueChoice>& GetDialogueChoices() const;
+
+	UFUNCTION(BlueprintCallable)
+	void AddDialogueChoice(const FDialogueChoice& DialogueChoice);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveDialogueChoiceAt(int32 Index);
+	
 	bool IsDialogueAlreadyShown() const;
 	void SetShownCondition(const bool NewCondition);
 	
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	FText Title;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	ENPCID SpeakerID;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	FText DialogueText;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TArray<FDialogueChoice> DialogueResponses;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TArray<FDialogueChoice> DialogueChoices;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	bool bIsShown;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Quest")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Quest", meta = (AllowPrivateAccess = "true"))
 	TSubclassOf<UQuestBase> QuestToGive;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Quest")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Quest", meta = (AllowPrivateAccess = "true"))
 	TSubclassOf<UQuestBase> QuestToClear;
 };
