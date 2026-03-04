72719849bc494b3d875272f615581c1dcb12bcff
YosephKim <kjs940207@gmail.com>
2026-03-04 15:02:46 +0900

    (불확실) 프로젝트를 플러그인화 한 이후 빌드 실패때 추가한 코드들

--- Plugins/DialogueKit/Source/DialogueKit/DialogueKit.Build.cs
+++ Plugins/DialogueKit/Source/DialogueKit/DialogueKit.Build.cs
@@ -7,10 +7,16 @@ public class DialogueKit : ModuleRules
 	public DialogueKit(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+		
+		// IWYU 도구가 실제로 코드를 수정/적용하도록 허용하는 수준 지정
+		IWYUSupport = IWYUSupport.None; // 엔진 버전에 따라 사용
+		bUseUnity = false;
+		
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags", "UMG", "DeveloperSettings" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		
 		
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
